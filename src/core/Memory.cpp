764c4bd01966235effe356e81da891d2c222f5e1
Davide Caroselli <davide@modernmt.eu>
2023-03-12 09:32:48 +0100

    Fix memory access on empty data

--- src/core/Memory.cpp
+++ src/core/Memory.cpp
@@ -10,13 +10,16 @@ Memory::Memory(uint32_t size, uint16_t begin, uint16_t end) : begin(begin), end(
 }
 
 uint8_t Memory::bus_read(uint8_t bus_id, uint16_t addr) {
+    if (data_.empty()) return 0x00;
     uint16_t idx = this->mapper ? this->mapper->remap(bus_id, addr) : ((addr - begin) & mask);
     return data_[idx];
 }
 
 void Memory::bus_write(uint8_t bus_id, uint16_t addr, uint8_t val) {
-    uint16_t idx = this->mapper ? this->mapper->remap(bus_id, addr) : ((addr - begin) & mask);
-    data_[idx] = val;
+    if (!data_.empty()) {
+        uint16_t idx = this->mapper ? this->mapper->remap(bus_id, addr) : ((addr - begin) & mask);
+        data_[idx] = val;
+    }
 }
 
 Memory::Memory(std::vector<uint8_t> memory, std::shared_ptr<Mapper> mapper, uint16_t begin, uint16_t end)
